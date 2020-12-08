891443a5f13f2253dbda358d4eaab63089fa3e00
vcwild <vcwildm@gmail.com>
2020-12-08 16:21:23 -0300

    update etl with gcp blob storage upload

--- etl.py
+++ etl.py
@@ -6,9 +6,14 @@ from tasks import *
 
 schedule = IntervalSchedule(interval=timedelta(minutes=5))
 
+bucket_name = "etl-demo-fractal"
+source_file_name = "./enade2019.csv"
+destination_blob_name = "enade2019-blob"
+
 def main():
   with Flow('enade-flow', schedule) as flow:
     path = get_raw_data()
+
     filters = apply_filters(path)
     estcivil = clean_estcivil(filters)
     cor = clean_cor(filters)
@@ -17,8 +22,12 @@ def main():
     renda = clean_renda(filters)
 
     joined_data = join_data(filters, estcivil, cor, escopai, escomae, renda)
-    end = write_csv(joined_data)
+    write = write_csv(joined_data)
+    end = upload_blob(bucket_name, source_file_name, destination_blob_name)
 
+    flow.set_dependencies(
+    task=end,
+    upstream_tasks=[write])
 
   flow.register(project_name='Enade', idempotency_key=flow.serialized_hash())
 
